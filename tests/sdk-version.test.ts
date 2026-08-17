fbf61b028e7af44364334032a1a5b9c80984ec65
Kojib <kojibchat@gmail.com>
2026-08-17 01:48:14 -0400

    test twin environment contract

--- tests/sdk-version.test.ts
+++ tests/sdk-version.test.ts
@@ -116,6 +116,7 @@ test("the production env template contains only standalone Wildz variables and a
     "NEXT_PUBLIC_WILDZ_SW_RELEASE",
     "WILDS_PULSE_TICK_SECRET",
     "RECEIZ_CONNECT_ACCESS_TOKEN",
+    "RECEIZ_CREATURE_TWIN_HANDLE",
     "NEXT_PUBLIC_CHECKOUT_MODE",
     "RECEIZ_CHECKOUT_MODE"
   ]);
